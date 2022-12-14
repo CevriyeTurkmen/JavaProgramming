a7166375a0e735cba23406a61281b83fb5812111
Cevriye Berkcan Turkmen <cberkcan@gmail.com>
2022-12-14 16:50:31 +0000

    day 38_Inheritance

--- JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
+++ JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
@@ -2,7 +2,16 @@ package day38_Inheritance.carTask;
 
 public class Toyota extends Car {
 
-    public Toyota(String brand, String model, int year, double price, String color, int miles) {
-        super(brand, model, year, price, color, miles);
+    public Toyota(String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
     }
+
+    public void reliable(){
+        System.out.println(brand+" "+ model+ "is reliable");
+
+    }
+
+
+
+
 }
